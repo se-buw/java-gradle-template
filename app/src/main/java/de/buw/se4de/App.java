f997378d942b36b3526f65eebc18b50d106a8526
Jan Oliver Ringert <jan.ringert@uni-weimar.de>
2022-04-20 17:22:29 +0200

    added CSV usage example

--- app/src/main/java/de/buw/se4de/App.java
+++ app/src/main/java/de/buw/se4de/App.java
@@ -3,9 +3,36 @@
  */
 package de.buw.se4de;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+    	String result = "";
+    	try (
+                Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/book.csv"));
+                @SuppressWarnings("deprecation")
+				CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                        .withFirstRecordAsHeader()
+                        .withIgnoreHeaderCase()
+                        .withTrim());
+            ) {
+                for (CSVRecord csvRecord : csvParser) {
+                    String name = csvRecord.get("author");
+                    result += "Hello " + name + "!\n";
+                }
+            } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	
+        return result;
     }
 
     public static void main(String[] args) {
